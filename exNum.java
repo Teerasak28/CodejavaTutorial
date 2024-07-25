9badd96f96ef1cebc4ade904265a681c692f8804
Teerasak28 <goodgametee@gmail.com>
2024-07-25 14:31:21 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x,y;
+        x = 69;
+        y = 99;
+        int total = x + y;
+        System.out.println("Total = "+total);
+        
+    }
+    
+}
