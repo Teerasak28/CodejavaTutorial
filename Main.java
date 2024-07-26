1cbb572221ddfbcf984e22459ce8c7b3bf55696c
Teerasak28 <goodgametee@gmail.com>
2024-07-26 14:46:52 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double A = 3.0; 
+        double B = 4.0; 
+        double F = Math.sqrt(Math.pow(A, 2) + Math.pow(B, 2));
+        System.out.println("Result (F): " + F);
+    }
+}
