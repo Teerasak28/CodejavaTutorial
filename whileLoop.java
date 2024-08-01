1215bbaabb146ccb31f9fbcb15f71303bf286cb2
Teerasak28 <goodgametee@gmail.com>
2024-08-01 10:09:51 +0700

    Create whileLoop.java

---
+++ whileLoop.java
@@ -0,0 +1,13 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++){
+            System.out.println(+i);
+        }
+        int iwhile = 1;
+        while (iwhile <= 100) {
+            System.out.println("result in whileLoop : "+iwhile);
+            iwhile += 2 ;
+        }
+    }
+    
+}
