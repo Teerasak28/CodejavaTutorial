322ca9262a9556083ac8a934ff0472ed96a32ecd
Teerasak28 <goodgametee@gmail.com>
2024-08-01 09:22:46 +0700

    Create TestFor.java

---
+++ TestFor.java
@@ -0,0 +1,8 @@
+public class TestFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 12; i++) { 
+            System.out.println("2 x " + i + " = " + (2 * i)); 
+        }
+    }
+}
+
