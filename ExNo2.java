bc217fa40832ecb368bf67a61414ed832a4dd911
Teerasak28 <goodgametee@gmail.com>
2024-08-01 14:17:29 +0700

    Create ExNo2.java

---
+++ ExNo2.java
@@ -0,0 +1,10 @@
+public class ExNo2 {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+    
+}
