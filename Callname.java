cb9384895fbd42748ee297a739ba57823aff570d
Teerasak28 <goodgametee@gmail.com>
2024-08-02 10:13:44 +0700

    Create Callname.java

---
+++ Callname.java
@@ -0,0 +1,11 @@
+public class Callname {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            myname();
+            }
+        }
+        public static void myname(){
+            System.out.println("Teerasak Saeheng");
+    }
+    
+}
