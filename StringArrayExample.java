ec7f75de90b20ab3050c4296fbfb833e865aa548
Teerasak28 <goodgametee@gmail.com>
2024-08-05 15:30:47 +0700

    Create StringArrayExample.java

---
+++ StringArrayExample.java
@@ -0,0 +1,12 @@
+public class StringArrayExample {
+    public static void main(String[] args) {
+        // สร้างอาร์เรย์ของสตริง
+        String[] strings = {"Hello", "World", "Java", "Programming", "is", "fun"};
+
+        // ใช้ for loop เพื่อแสดงผลข้อมูลในอาร์เรย์
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+        }
+    }
+}
+
