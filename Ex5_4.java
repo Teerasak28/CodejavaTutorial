87fa1e08f9905c05fc47fa956ebb296f46ac3a69
Teerasak28 <goodgametee@gmail.com>
2024-08-05 09:15:36 +0700

    Create Ex5_4.java

---
+++ Ex5_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex5_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("กรุณาป้อนตัวเลข: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " เป็นเลขคู่");
+        } else {
+            System.out.println(number + " เป็นเลขคี่");
+        }
+        scanner.close();
+    }
+}
